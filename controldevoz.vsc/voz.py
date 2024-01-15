dd533d39a92866d5b0f288a9255442a55b8289d2
PereiraAle <90768724+PereiraAle@users.noreply.github.com>
2024-01-15 01:49:44 -0300

    Update voz.py

--- controldevoz.vsc/voz.py
+++ controldevoz.vsc/voz.py
@@ -1,45 +1,44 @@
 import speech_recognition as sr
 import webbrowser
 
-# Inicializar el reconocimiento de voz
+
 r = sr.Recognizer()
 
-# Función para capturar y procesar el comando de voz
 def procesar_comando():
     with sr.Microphone() as source:
         print("Di algo...")
         audio = r.listen(source)
 
     try:
-        # Utilizar Google Speech Recognition para convertir el audio en texto
+      
         texto = r.recognize_google(audio, language='es')
         print("Has dicho: " + texto)
         
-        # Verificar si el comando es "abrir YouTube"
+      
         if "Abrir YouTube" in texto:
-            # Abrir YouTube en el navegador
+         
             webbrowser.open("https://www.youtube.com")
            
-             # Verificar si el comando es "abrir Google"
+        
         if "Abrir Google" in texto:
-            # Abrir Google en el navegador
-            webbrowser.open("https://www.google.com")
-
-  # Verificar si el comando es "abrir Google"
-        if "Abrir Google" in texto:
-            # Abrir Google en el navegador
+         
             webbrowser.open("https://www.google.com")
 
 
-        # Verificar si el comando es "Cerrar asistente"
+        if "Abrir Google" in texto:
+       
+            webbrowser.open("https://www.google.com")
+
+
+
         if "cerrar asistente" in texto:
-            # Detener el asistente y salir del bucle
+      
             print("Cerrando asistente...")
             return True
         
-        # Verificar si el comando es "abrir fútbol"
+   
         if "abrir fútbol" in texto:
-            # Abrir Google en el navegador
+         
             webbrowser.open("https://www.haxball.com/")
 
 
@@ -51,7 +50,7 @@ def procesar_comando():
 
     return False
 
-# Ejecutar el procesamiento del comando de voz continuamente
+
 while True:
     if procesar_comando():
         break
